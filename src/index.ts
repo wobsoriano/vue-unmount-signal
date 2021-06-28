f3cbf1ae7ca7c93a163988e84ab09b4271ee4eec
Robert Soriano <sorianorobertc@gmail.com>
2021-06-28 12:30:06 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { onUnmounted } from 'vue-demi'
+
+/**
+ * A Vue Hook that returns an AbortSignal that is marked as aborted when the calling component is
+ * unmounted. This is useful for canceling promises, such as those for network requests, when a
+ * component is unmounted.
+ */
+export default function useUnmountSignal(): AbortSignal {
+    const abortController = new AbortController()
+
+    onUnmounted(() => {
+        abortController.abort()
+    })
+
+    return abortController.signal
+}
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'useUnmountSignal'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    exclude: ['vue-demi']
+  }
+})
