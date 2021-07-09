ea467710f42ba09ac2a55c76a6eb63d5a88e9816
Robert Soriano <sorianorobertc@gmail.com>
2021-07-09 20:50:29 +0800

    add tests

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: 'ts-jest',
+    testRegex: '/test/.+\\.spec\\.(js|ts)$',
+    moduleFileExtensions: ['ts', 'js'],
+}
---
+++ test/v-use-unmount-signal.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from 'vue-composable-tester'
+import { nextTick } from 'vue'
+import useUnmountSignal from '../src'
+
+test(`it returns an AbortSignal`, () => {
+    const { result } = mount(() => useUnmountSignal())
+    expect(result).toBeInstanceOf(AbortSignal);
+});
+
+test(`it marks the AbortSignal as aborted when unmounted`, () => {
+    const { result, unmount } = mount(() => useUnmountSignal());
+    expect(result.aborted).toBe(false);
+    unmount();
+    expect(result.aborted).toBe(true);
+});
+  
+test(`it does not change the AbortSignal when re-rendered`, async () => {
+    const { result } = mount(() => useUnmountSignal());
+    const initialAbortSignal = result;
+    await nextTick();
+    expect(result).toBe(initialAbortSignal);
+});
