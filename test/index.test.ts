a95b2b96ce46aa4f80abb4e14a1579c8414a21de
Robert Soriano <sorianorobertc@gmail.com>
2022-03-10 23:39:40 -0800

    update composable tester

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,22 +1,22 @@
-import { mount } from 'vue-composable-tester'
+import { renderComposable } from 'vue-test-composables'
 import { nextTick } from 'vue'
 import useUnmountSignal from '../src'
 import { test, expect } from 'vitest'
 
 test(`it returns an AbortSignal`, () => {
-  const { result } = mount(() => useUnmountSignal())
+  const { result } = renderComposable(() => useUnmountSignal())
   expect(result).toBeInstanceOf(AbortSignal)
 })
 
 test(`it marks the AbortSignal as aborted when unmounted`, () => {
-  const { result, unmount } = mount(() => useUnmountSignal())
+  const { result, unmount } = renderComposable(() => useUnmountSignal())
   expect(result.aborted).toBe(false)
   unmount()
   expect(result.aborted).toBe(true)
 })
 
 test(`it does not change the AbortSignal when re-rendered`, async () => {
-  const { result } = mount(() => useUnmountSignal())
+  const { result } = renderComposable(() => useUnmountSignal())
   const initialAbortSignal = result
   await nextTick()
   expect(result).toBe(initialAbortSignal)
